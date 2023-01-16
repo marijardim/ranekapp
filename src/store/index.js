9ab932f58d1d7f7e53a77210b16e194f6c72c0f6
unknown <mari-jen@hotmail.com>
2023-01-16 16:15:03 -0300

    produtos-lista

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,39 @@
+import { api } from '@/services/index.js'
 import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    login: false,
+    user: {
+      id: '',
+      name: '',
+      email: '',
+      password: '',
+      postal: '',
+      address: '',
+      number: '',
+      city: '',
+      state: ''
+    }
   },
   getters: {
   },
   mutations: {
+    UPDATE_LOGIN (state, payload) {
+      state.login = payload
+    },
+    UPDATE_USER (state, payload) {
+      state.user = payload
+    }
   },
   actions: {
+    getUser (context, payload) {
+      api.get(`/user/${payload}`)
+        .then(response => {
+          context.commit('UPDATE_USER', response.data)
+          context.commit('UPDATE_LOGIN', true)
+        })
+    }
   },
   modules: {
   }
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,13 +1,71 @@
 <template>
-  <div>Login</div>
+  <section class="login">
+    <h1>Login</h1>
+    <form>
+      <label for="email">Email</label>
+      <input type="email" name="email" id="email" v-model='login.email'>
+      <label for="password">Senha</label>
+      <input type="password" name="password" id="password" v-model='login.password'>
+      <button class="btn" @click.prevent='logar'>Logar</button>
+    </form>
+    <p class="lostPassword">
+      <a href="/" target="_blank">Perdeu a senha? Clique aqui.</a>
+    </p>
+    <LoginCreate />
+  </section>
 </template>
 
 <script>
-export default {
+import LoginCreate from '@/components/LoginCreate'
 
+export default {
+  name: 'LoginView',
+  components: { LoginCreate },
+  data () {
+    return {
+      login: {
+        email: '',
+        password: ''
+      }
+    }
+  },
+  methods: {
+    logar () {
+      this.$store.dispatch('getUser', this.login.email)
+      this.$router.push({ name: 'user' })
+    }
+  }
 }
 </script>
 
-<style>
+<style scoped>
+.login {
+  max-width: 500px;
+  margin: 0 auto;
+  padding: 0 20px;
+}
+form {
+  display: grid;
+}
+.btn{
+  width: 100%;
+  max-width: 300px;
+  margin-left: auto;
+  margin-right: auto;
+}
+.lostPassword{
+  text-align: center;
+  margin: 20px auto 0 auto;
+}
+.lostPassword a:hover{
+  color: #87f;
+  text-decoration: underline;
+}
+h1{
+  text-align: center;
+  font-size: 2rem;
+  margin-top: 40px;
+  color: #87f;
+}
 
 </style>
